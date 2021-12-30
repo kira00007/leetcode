336352c1c339e01e42f439ce69b5865cd6bb4311
Asad Khan <asad104khan@gmail.com>
2021-12-30 11:29:47 +0530

    prob-6 working solution best formatting

--- prob-6.cpp
+++ prob-6.cpp
@@ -7,29 +7,17 @@ public:
 		const int l = s.size();
 		if(n == 1 || l <= n) return s;
 		string ans;
-		int inc = 2*n - 2;
-		int st = 0;
-		while(st < l) {
-			ans.push_back(s[st]);
-			st += inc;
-		}
+        for(int st = 0; st < l; st += (2*n-2))
+            ans.push_back(s[st]);
 		for(int i = 1; i < (n-1); i++) {
-			st = i;
-			int inc1 = 2*(n-i-1);
-			int inc2 = 2*i;
-			bool swtch = false;
-			while(st < l) {
-				ans.push_back(s[st]);
-				st += swtch ? inc2 : inc1;
-				swtch = !swtch;
-			}
+            for(int st = i; st < l; st += 2*i) {
+                ans.push_back(s[st]);
+                st += 2*(n-i-1);
+                if(st < l) ans.push_back(s[st]);
+            }
 		}
-		st = n-1;
-		// inc = 2*n - 2;
-		while(st < l) {
-			ans.push_back(s[st]);
-			st += inc;
-		}
+        for(int st = n-1; st < l; st += (2*n-2))
+            ans.push_back(s[st]);
 		return ans;
 	}
 };
